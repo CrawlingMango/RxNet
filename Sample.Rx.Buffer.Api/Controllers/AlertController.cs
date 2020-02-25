46177ece2ee43114545aa1b0f5a2133664776fc3
USER <USER@DESKTOP-IQL7V82>
2020-02-26 00:35:09 +0800

    added alert service and other files

---
+++ Sample.Rx.Buffer.Api/Controllers/AlertController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sample.Rx.Buffer.Api.Services;
+
+namespace Sample.Rx.Buffer.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlertController : ControllerBase
+    {
+        private readonly AlertService _alertService;
+
+        public AlertController(AlertService alertService)
+        {
+            _alertService = alertService;
+        }
+
+        [HttpGet("ping")]
+        public ActionResult<string> Ping() 
+        {
+            _alertService.Alert("<error-message>");
+            return Ok($"Pong");
+        }
+    }
+}
---
+++ Sample.Rx.Buffer.Api/Services/AlertService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Sample.Rx.Buffer.Api.Services
+{
+    /*
+     * will log after 5 seconds
+     * if after five seconds and the buffer is not full, it will not log
+     */
+    public class AlertService
+    {
+        private readonly ILogger<AlertService> _logger;
+
+        public static BehaviorSubject<string> _obsAlertMessage = new BehaviorSubject<string>("first-alert");
+
+        public AlertService(ILogger<AlertService> logger)
+        {
+            _logger = logger;
+
+            Observable.AsObservable(_obsAlertMessage)
+                .Delay(new TimeSpan(0, 0, 5))
+                .Buffer(5)
+                .Subscribe(m => _logger.LogInformation(string.Join("|", m)));
+        }
+
+        public void Alert(string message)
+        {
+            _obsAlertMessage.OnNext(message);
+        }
+    }
+}
